98f543c83e0e805263efa9f3e465535ad25e973d
ftarbash <fatma@arbash.co>
2022-08-08 11:53:06 +0300

    cw3

--- cw3.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-var favouriteNumber:Int = 9
-var name:String = "fatma"
-var hobby = "fencing"
-let BirthOfDate = 2005
-print("hello i'm \(name) and my favourite number is \(favouriteNumber) and my hobby is \(hobby) \n and i was born at \(BirthOfDate)")
-
